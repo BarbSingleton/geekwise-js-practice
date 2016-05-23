fd252e7ba5311dd8462ced5fbe5cf18984c9eb76
Barb Singleton <BarbSingleton@gmail.com>
2016-05-23 16:36:37 -0700

    clean up to work console

---
+++ day1js.js
@@ -0,0 +1,10 @@
+var myFavoriteNumber = 22;
+var notMyFavoriteNumber = "66";
+var average;
+
+console.log("My favorite number is: " + myFavoriteNumber + ", it's not: " + notMyFavoriteNumber);
+console.log(typeof myFavoriteNumber);
+console.log(typeof notMyFavoriteNumber);
+console.log("this is a random string");
+average = (myFavoriteNumber + Number(notMyFavoriteNumber)) / 2;
+console.log(average);
